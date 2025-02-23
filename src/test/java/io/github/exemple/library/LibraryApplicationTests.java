0790da45b2d90599e84223f35693be7981a57ce7
Rodrigo Pereira <rodrigogr.pereira@gmail.com>
2025-02-23 13:27:00 -0300

    Initial commit conexao

---
+++ src/test/java/io/github/exemple/library/LibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.exemple.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
